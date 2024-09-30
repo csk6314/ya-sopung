9d5371a0f26a0e705936f5561ced39e327dbdc7d
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-09-30 14:37:24 +0900

    chore: popular 더미데이터 추가

---
+++ src/constant/popular.ts
@@ -0,0 +1,32 @@
+export const POPULAR = [
+  {
+    rank: 1,
+    keyword: "서울",
+    src: "https://i.namu.wiki/i/vzQIsovYmymvt84FWS9hhAS3Qva_O31fczg3ErMrSQrSgnJ9h3oQDf3kVo5M6aD1Q05-etvQRlqc3dnurUczXw.webp",
+  },
+  {
+    rank: 2,
+    keyword: "여수",
+    src: "https://i.namu.wiki/i/aopT1LAMmS9TbyHnurX57GpQp7kduzXG45NPEG2hN9xA16_CksflefDu_m3R5PsJnriOrVM_hus80_4CmQAizg.webp",
+  },
+  {
+    rank: 3,
+    keyword: "부산",
+    src: "https://i.namu.wiki/i/hkDOgJHC40yiIFKQDRz7YjHpzWrL9vCTT7mve4TF6Lj-GpGsBpvT8WlXbwOT_To1Ndl1zKrVLQ-SiwaGNFOgQA.webp",
+  },
+  {
+    rank: 4,
+    keyword: "속초",
+    src: "https://i.namu.wiki/i/VU1u3fYqWTvug6xME7sBR8o76KU-abzg2yfKa2dtYjT7Xe0YveLq9lAFN7bM5R0bYAYZTCaaDAiRwz6Rruq1PQ.webp",
+  },
+  {
+    rank: 5,
+    keyword: "강릉",
+    src: "https://www.gtdc.or.kr/dzSmart/upfiles//site/2024May/20/16d0487ba2f051f9e78cd5be3f1649a4_1715970797.jpg",
+  },
+  {
+    rank: 6,
+    keyword: "제주",
+    src: "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/6dTBQAAJW6QLbLUaneCZWL3Vhf4.jpg",
+  },
+];
