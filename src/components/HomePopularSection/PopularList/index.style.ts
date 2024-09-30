f55202d4f3196624dcb39c07de8b537ca561e7cd
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-09-30 14:37:10 +0900

    feat: PopularList(AreaCardList) 컴포넌트 구현

---
+++ src/components/HomePopularSection/PopularList/index.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SwiperWrapper = styled.div`
+  width: 100%;
+
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    padding: 0.5rem;
+  }
+`;
---
+++ src/components/HomePopularSection/PopularList/index.tsx
@@ -0,0 +1,47 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+//component
+import AreaCard from "../AreaCard";
+
+//style
+import { SwiperWrapper } from "./index.style";
+
+//constant
+import { POPULAR } from "@/constant/popular";
+
+const PopularList = () => {
+  return (
+    <SwiperWrapper>
+      <Swiper
+        slidesPerView={1}
+        breakpoints={{
+          1280: {
+            slidesPerView: 6,
+            spaceBetween: 16,
+          },
+          1060: {
+            slidesPerView: 5,
+            spaceBetween: 16,
+          },
+          800: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+          },
+          600: {
+            slidesPerView: 3,
+            spaceBetween: 1,
+          },
+        }}
+      >
+        {POPULAR.map((popular) => (
+          <SwiperSlide key={popular.keyword}>
+            <AreaCard src={popular.src} keyword={popular.keyword} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </SwiperWrapper>
+  );
+};
+
+export default PopularList;
