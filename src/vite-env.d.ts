30de024af392c89d65bd368579c0adc69033a82e
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-02 18:28:51 +0900

    chore: vite env setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_ENCODING_KEY: string;
+  readonly VITE_API_DECODING_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
