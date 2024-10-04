07dafbc7e3613d344d861b77ef40f982ccc1a18f
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-04 11:25:17 +0900

    feat: QueryClient 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -9,50 +9,64 @@ import TouristAttrPage from "./pages/tourist-attr";
 import MyPage from "./pages/my";
 import NavBanner from "./components/NavBanner";
 import DetailPage from "./pages/detail";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "",
+        element: <NavBanner />,
+        children: [
+          {
+            path: "festival",
+            element: <FestivalPage />,
+          },
+          {
+            path: "accommodation",
+            element: <AccommodationPage />,
+          },
+          {
+            path: "tourist-attr",
+            element: <TouristAttrPage />,
+          },
+        ],
+      },
+      {
+        path: "detail/:id",
+        element: <DetailPage />,
+      },
+      {
+        path: "my",
+        element: <MyPage />,
+      },
+    ],
+  },
+]);
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "",
-          element: <HomePage />,
-        },
-        {
-          path: "",
-          element: <NavBanner />,
-          children: [
-            {
-              path: "festival",
-              element: <FestivalPage />,
-            },
-            {
-              path: "accommodation",
-              element: <AccommodationPage />,
-            },
-            {
-              path: "tourist-attr",
-              element: <TouristAttrPage />,
-            },
-          ],
-        },
-        {
-          path: "detail/:id",
-          element: <DetailPage />,
-        },
-        {
-          path: "my",
-          element: <MyPage />,
-        },
-      ],
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        throwOnError: true,
+        refetchOnWindowFocus: false,
+        retry: 0,
+      },
     },
-  ]);
-
+  });
   return (
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
