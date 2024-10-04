1150c3f1bfb78e2a0550047afabfaa618e6f9ce4
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-04 15:15:06 +0900

    feat: useParams hook 구현

---
+++ src/hooks/useParams/index.ts
@@ -0,0 +1,18 @@
+import { AREA_CODE } from "@/constant/area";
+import { ROUTES } from "@/constant/routes";
+import { useLocation, useSearchParams } from "react-router-dom";
+
+const useParams = () => {
+  const { pathname } = useLocation();
+  const [searchParam] = useSearchParams();
+
+  const contentType =
+    ROUTES.find((route) => route.href === pathname)?.contentType ?? "15";
+  const keyword = searchParam.get("keyword") ?? "";
+  const code = searchParam.get("area") ?? "0";
+  const area = AREA_CODE.find((ac) => ac.code === code) ?? AREA_CODE[0];
+
+  return { contentType, keyword, area };
+};
+
+export default useParams;
