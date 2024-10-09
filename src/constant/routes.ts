d30baec94787ade35b63923fa7e0c9318fc0b557
csk6314 <37896060+csk6314@users.noreply.github.com>
2024-10-09 19:21:18 +0900

    chore: ROUTES icon 데이터 추가

--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -1,9 +1,14 @@
+import { IconType } from "react-icons";
 import { TContentType } from "./content";
+import { MdAttractions, MdFestival } from "react-icons/md";
+import { TiHome } from "react-icons/ti";
+import { FaUser } from "react-icons/fa6";
 
 interface IRoute {
   href: string;
   title: string;
   contentType?: TContentType;
+  icon?: IconType;
 }
 
 export const ROUTES: IRoute[] = [
@@ -15,19 +20,23 @@ export const ROUTES: IRoute[] = [
     href: "/festival",
     title: "행사",
     contentType: "15",
+    icon: MdFestival,
   },
   {
     href: "/accommodation",
     title: "숙박",
     contentType: "32",
+    icon: TiHome,
   },
   {
     href: "/tourist-attr",
     title: "관광지",
     contentType: "12",
+    icon: MdAttractions,
   },
   {
     href: "/my",
     title: "마이페이지",
+    icon: FaUser,
   },
 ];
